8accc0b480e6e75212c3629e37b75574c4c344ee
Joaostopa <120514903+Joaostopa@users.noreply.github.com>
2024-09-09 13:28:23 -0300

    Add files via upload

---
+++ backend/logger.js
@@ -0,0 +1,22 @@
+const { createLogger, format, transports } = require('winston');
+const { combine, timestamp, printf } = format;
+
+
+const myFormat = printf(({ level, message, timestamp }) => {
+    return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
+
+
+const logger = createLogger({
+    level: 'info',
+    format: combine(
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: 'logs/app.log' }) 
+    ]
+});
+
+module.exports = logger;
